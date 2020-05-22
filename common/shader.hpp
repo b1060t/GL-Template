cadfd1fd8d7a48524576fdb87488cd2d90b85fa9
biobots <biobot@qq.com>
2020-05-22 14:59:52 +0800

    Embedding shaders
    Signed-off-by: biobots <biobot@qq.com>

--- common/shader.hpp
+++ common/shader.hpp
@@ -1,6 +1,7 @@
 #ifndef SHADER_HPP
 #define SHADER_HPP
 
-GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path);
+GLuint LoadShaders(std::string vertex_file,std::string fragment_file);
+GLuint LoadExternalShaders(const char * vertex_file_path,const char * fragment_file_path);
 
 #endif
--- main/test.cpp
+++ main/test.cpp
@@ -1,6 +1,7 @@
 // Include standard headers
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
 
 // Include GLEW
 #include <GL/glew.h>
@@ -17,6 +18,15 @@ GLFWwindow* window;
 #include <shader.hpp>
 
 using namespace glm;
+
+static const GLfloat buffer_data[] = {
+    -1.0f, -1.0f, 0.0f,
+    1.0f, -1.0f, 0.0f,
+    0.0f, 1.0f, 0.0f,
+};
+
+extern const char _binary_shaders_test_vert_start, _binary_shaders_test_vert_end;
+extern const char _binary_shaders_test_frag_start, _binary_shaders_test_frag_end;
 
 int main( void )
 {
@@ -58,12 +68,32 @@ int main( void )
 	// Dark blue background
 	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
 
+    GLuint VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    GLuint VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(buffer_data), buffer_data, GL_STATIC_DRAW);
+
+    GLuint programID = LoadShaders(
+        std::string(&_binary_shaders_test_vert_start, &_binary_shaders_test_vert_end - &_binary_shaders_test_vert_start),
+        std::string(&_binary_shaders_test_frag_start, &_binary_shaders_test_frag_end - &_binary_shaders_test_frag_start)
+        );
+
 	do{
 		// Clear the screen. It's not mentioned before Tutorial 02, but it can cause flickering, so it's there nonetheless.
-		glClear( GL_COLOR_BUFFER_BIT );
+		glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// Draw nothing, see you in tutorial 2 !
-
+        glUseProgram(programID);
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glVertexAttribPointer(
+            0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0
+        );
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDisableVertexAttribArray(0);
 		
 		// Swap buffers
 		glfwSwapBuffers(window);
@@ -72,6 +102,10 @@ int main( void )
 	} // Check if the ESC key was pressed or the window was closed
 	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
 		   glfwWindowShouldClose(window) == 0 );
+
+    glDeleteBuffers(1, &VBO);
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteProgram(programID);
 
 	// Close OpenGL window and terminate GLFW
 	glfwTerminate();

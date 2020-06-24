58b6dcfca153702a1bdfddd14f915f396f7bd015
Biobots <biobot@qq.com>
2020-06-24 19:48:04 +0800

    add imgui support
    Signed-off-by: Biobots <biobot@qq.com>

--- example/geometry.cpp
+++ example/geometry.cpp
@@ -28,6 +28,9 @@
 #include <assimp/postprocess.h>
 
 #include <vector>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 
 using namespace glm;
 using namespace tide;
@@ -84,15 +87,60 @@ int main()
 	Element c(const_cast<GLfloat*>(&circles[0]), const_cast<GLint*>(&indices[0]), sizeof(circles), sizeof(indices), CIRCLE_ATTR);
 	c.attachShader(&circle_shader);
 
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+	ImGui::StyleColorsDark();
+	ImGui_ImplGlfw_InitForOpenGL(context.getWindow(), true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+	bool show_demo_window = true;
+    bool show_another_window = false;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
 	do
 	{
+		ImGui_ImplOpenGL3_NewFrame();
+    	ImGui_ImplGlfw_NewFrame();
+    	ImGui::NewFrame();
+		if (show_demo_window)
+            ImGui::ShowDemoWindow(&show_demo_window);
+
+		{
+            static float f = 0.0f;
+            static int counter = 0;
+
+            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+            ImGui::Checkbox("Another Window", &show_another_window);
+
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::End();
+        }
+		
 		glClear(GL_COLOR_BUFFER_BIT);
         glClearColor(0.2f, 0.2f, 0.2f, 0.0f);
 
 		e.render(GL_POINTS);
 		c.render(GL_POINTS);
 
+		ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
 		context.loop();
 	}
 	while(context.shouldClose(GLFW_KEY_ESCAPE));
+
+	ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 }

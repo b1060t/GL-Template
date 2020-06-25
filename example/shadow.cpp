d79085f864c4f4d7b04a36086fd46cdf57617380
Biobots <biobot@qq.com>
2020-06-25 09:27:18 +0800

    update cmake
    Signed-off-by: Biobots <biobot@qq.com>

--- example/shadow.cpp
+++ example/shadow.cpp
@@ -29,6 +29,10 @@ using namespace tide;
 #define WIDTH 1024
 #define HEIGHT 768
 
+EXTLD(shaders_tex_vert);
+EXTLD(shaders_tex_frag);
+EXTLD(misc_wood_png);
+
 int main()
 {
 	Context context("Shadow");
@@ -37,9 +41,32 @@ int main()
 	Camera cam(context.getWindow(), WIDTH, HEIGHT, 45.0f, glm::vec3(0,0,0));
     glfwSetCursorPos(context.getWindow(), WIDTH/2, HEIGHT/2);
 
+    mango::Memory img_mem((unsigned char*)LDVAR(misc_wood_png), LDLEN(misc_wood_png));
+    mango::Bitmap img_tex(img_mem, ".png", mango::FORMAT_B8G8R8);
+    GLuint tex;
+    glGenTextures(1, &tex);
+	glBindTexture(GL_TEXTURE_2D, tex);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img_tex.width, img_tex.height, 0, GL_BGR, GL_UNSIGNED_BYTE, img_tex.image);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+    Shader tex_shader(
+	    std::string(LDVAR(shaders_tex_vert), LDLEN(shaders_tex_vert)),
+	    std::string(LDVAR(shaders_tex_frag), LDLEN(shaders_tex_frag))
+	);
+
+    Element plane(const_cast<GLfloat*>(&ImageVertices[0]), const_cast<GLint*>(&ImageIndices[0]), sizeof(ImageVertices), sizeof(ImageIndices), TWOD_TEXTURE_ATTR);
+    plane.attachShader(&tex_shader);
+    plane.texdic["tex"] = tex;
+    plane.texdic["width"] = img_tex.width;
+    plane.texdic["height"] = img_tex.height;
+    plane.internal_model = false;
+
 	do
     {
         cam.loop();
+
+        plane.render();
         
         context.loop();
     }
